f3e038bfed4282c46d9c5ea2345f8580a5501e58
Ashish Chaudhari <122347602+ashishchaudhari1857@users.noreply.github.com>
2023-03-22 10:33:46 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,5 @@
+var title = document.getElementById("header-title");
+title.style.borderBottom = '2px solid black';
+var title2 = document.getElementsByClassName("title")[0];
+title2.style.fontWeight = 'bold';
+title2.style.color = 'green';
