dd0d54676c69f4cf7373cd94ad3de00b1bcc4183
Ashish Chaudhari <122347602+ashishchaudhari1857@users.noreply.github.com>
2023-03-22 12:45:59 +0530

    second assignment taks 4 of Dom

--- my.js
+++ my.js
@@ -3,3 +3,8 @@ title.style.borderBottom = '2px solid black';
 var title2 = document.getElementsByClassName("title")[0];
 title2.style.fontWeight = 'bold';
 title2.style.color = 'green';
+var item = document.getElementsByClassName("list-group-item");
+item[2].style.backgroundColor = 'green';
+for (var i = 0; i < item.length; i++) {
+    item[i].style.fontWeight = 'bold';
+}
